ee60f3f5f2e5b361377893b60423b6e1b339c007
luhhii <luhihernandez07@gmail.com>
2025-05-18 23:21:27 -0600

    Primer commit con flujo Docker

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('¡Hola desde Docker!');
+});
+
+server.listen(3000, () => {
+  console.log('Servidor en http://localhost:3000');
+});
